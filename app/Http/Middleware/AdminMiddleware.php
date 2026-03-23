ec43884ae3927672725179257016fbc87f6d42cc
ArpitSojitra7179 <arpitsojitra7179@gmail.com>
2026-03-23 09:53:15 +0530

    add Middleware for admin

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = Auth::guard('api')->user();
+
+        if (! $user || $user->role !== 'admin') {
+            return response()->json([
+                'message' => 'You are not an Admin, So you cannot access this route.'
+            ], 403);
+        }
+
+        return $next($request);
+    }
+}
