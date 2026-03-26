528433d509d80344c554a06c6659066b6887537f
ArpitSojitra7179 <arpitsojitra7179@gmail.com>
2026-03-26 15:02:17 +0530

    add event store api

--- app/Http/Controllers/Organizer/OrganizerController.php
+++ app/Http/Controllers/Organizer/OrganizerController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\Organizer;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Interfaces\EventRepositoryInterface;
+use App\Models\User;
+use App\Models\Event;
 
 class OrganizerController extends Controller
 {
@@ -18,5 +21,54 @@ class OrganizerController extends Controller
     public function index()
     {
         return $this->eventRepo->getAllEventCategories();
+    }
+
+    public function createEvent(Request $request, User $user)
+    {
+        $validated = $request->validate([
+            'category_id' => 'required|integer',
+            'title' => 'required|string',
+            'description' => 'required|string',
+            'location' => 'required|string',
+            'event_date' => 'required|date',
+            'ticket_price' => 'required|numeric',
+            'total_tickets' => 'required|integer',
+            'available_tickets' => 'required|integer',
+            'image' => 'required|image|mimes:jpeg,jpg,png,gif,svg|max:2048',
+        ]);
+
+        try {
+            $user = auth()->user();
+
+            if ($request->hasFile('image')) {
+                $imagePath = $request->file('image')->store('events', 'public');
+                $validated['image'] = $imagePath;
+            }
+
+            $event = Event::updateOrCreate([
+                'user_id' => $user->id,
+                'category_id' => $validated['category_id'],
+                'title' => $validated['title'],
+                'description' => $validated['description'],
+                'location' => $validated['location'],
+                'event_date' => $validated['event_date'],
+                'ticket_price' => $validated['ticket_price'],
+                'total_tickets' => $validated['total_tickets'],
+                'available_tickets' => $validated['available_tickets'],
+                'image' => $validated['image'],
+            ]);
+
+            return response()->json([
+                'message' => 'Event created successfully.',
+                'event' => $event,
+                'image_url' => asset('storage/' . $event->image),
+            ], 200);
+        } catch (\Exception $e) {
+            report($e);
+
+            return response()->json([
+                'message' => 'Something Went Wrong.',
+            ], 500);
+        }
     }
 }
--- routes/Organizer/organizer.php
+++ routes/Organizer/organizer.php
@@ -6,4 +6,5 @@ use App\Http\Controllers\Organizer\OrganizerController;
 
 Route::controller(OrganizerController::class)->middleware(['auth:api', 'IsOrganizer'])->group(function () {
 	Route::get('/all-categories', 'index');
+	Route::post('/create-event', 'createEvent');
 });
