b8b4a4656d9523bc5e8262d2497e79509aff0415
ArpitSojitra7179 <arpitsojitra7179@gmail.com>
2026-04-01 15:25:18 +0530

    move events api to other controller

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -5,19 +5,14 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 use App\Models\UserMeta;
 use App\Models\EventCategory;
 use App\Interfaces\EventRepositoryInterface;
 
 class AdminController extends Controller
 {
-    protected $eventRepo;
-
-    public function __construct(EventRepositoryInterface $eventRepo)
-    {
-        $this->eventRepo = $eventRepo;
-    }
-
+    
     public function requestList(Request $request) {
         try {
             $user = auth()->user();
@@ -59,32 +54,4 @@ class AdminController extends Controller
             ], 500);
         }
     }
-
-    public function setEventCategories(Request $request)
-    {
-        $request->validate([
-            'name' => 'required|string',
-        ]);
-
-        try {
-            $setCategory = EventCategory::create([
-                'name' => $request->name,
-            ]);
-
-            return response()->json([
-                'message' => 'Event category set successfully.',
-            ], 200);
-        } catch (\Exception $e) {
-            report($e);
-
-            return response()->json([
-                'message' => 'Something Went Wrong.',
-            ], 500);
-        }
-    }
-    
-    public function eventCategoryIndex()
-    {
-        return $this->eventRepo->getAllEventCategories();
-    }
-}   
+}
