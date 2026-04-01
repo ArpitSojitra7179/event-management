2de9917f2b8bec4f2de76f663367babd022afbc6
ArpitSojitra7179 <arpitsojitra7179@gmail.com>
2026-04-01 15:35:04 +0530

    add events routes

--- routes/Admin/admin.php
+++ routes/Admin/admin.php
@@ -4,16 +4,21 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\EventController;
 
 Route::controller(AdminController::class)->middleware(['auth:api', 'IsAdmin'])->group(function () {
     Route::get('/organizer-request-list', 'requestList');
     Route::patch('/approve-request/{user}', 'approveRequest');
-    Route::post('/set-event-category', 'setEventCategories');
-    Route::get('/get-event-category-list', 'eventCategoryIndex');
 });
 
 Route::controller(UserController::class)->middleware(['auth:api', 'IsAdmin'])->group( function () {
     Route::get('/get-all-users', 'index');
     Route::patch('/edit-user-data/{user}', 'update');
     Route::patch('/ban-user/{user}', 'banUser');
+});
+
+Route::controller(EventController::class)->middleware(['auth:api', 'IsAdmin'])->group(function () {
+    Route::post('/set-event-category', 'setEventCategories');
+    Route::get('/get-event-category-list', 'eventCategoryIndex');
+    Route::get('/get-all-events', 'index');
 });
