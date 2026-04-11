7ebf9ecf9bc022ba26cc6c95e0ce3369f2881ce8
ArpitSojitra7179 <arpitsojitra7179@gmail.com>
2026-04-11 10:26:57 +0530

    add event publish request route

--- routes/Organizer/organizer.php
+++ routes/Organizer/organizer.php
@@ -3,8 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Organizer\OrganizerController;
+use App\Http\Controllers\Organizer\EventApproveController;
 
 Route::controller(OrganizerController::class)->middleware(['auth:api', 'IsOrganizer'])->group(function () {
 	Route::get('/all-categories', 'index');
 	Route::post('/create-event', 'createEvent');
+});
+
+Route::controller(EventApproveController::class)->middleware(['auth:api', 'IsOrganizer'])->group(function () {
+	Route::post('event-publish-request', 'eventPublishRequest');
 });
