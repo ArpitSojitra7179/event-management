8c91d22db842867b4e15568a50eeda7470d28fda
ArpitSojitra7179 <arpitsojitra7179@gmail.com>
2026-06-08 12:02:09 +0530

    cretae CURD and event request api

--- routes/Organizer/organizer.php
+++ routes/Organizer/organizer.php
@@ -2,14 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Organizer\OrganizerController;
+use App\Http\Controllers\Organizer\EventController;
 use App\Http\Controllers\Organizer\EventApproveController;
 
-Route::controller(OrganizerController::class)->middleware(['auth:api', 'IsOrganizer'])->group(function () {
-	Route::get('/all-categories', 'index');
-	Route::post('/create-event', 'createEvent');
-});
-
-Route::controller(EventApproveController::class)->middleware(['auth:api', 'IsOrganizer'])->group(function () {
-	Route::post('event-publish-request', 'eventPublishRequest');
+Route::controller(EventController::class)->middleware(['auth:api', 'IsOrganizer'])->group(function () {
+	Route::get('/categories', 'categoryIndex');
+	Route::post('/events', 'store');
+	Route::post('/events/{event}/request', 'eventRequest');
+	Route::get('/events', 'index');
+	Route::get('/events/{event}', 'show');
+	Route::patch('/events/{event}', 'update');
+	Route::delete('/events/{event}', 'destroy');
 });
