8141065e41583a4935c8ad73b143a07d1df37edf
ArpitSojitra7179 <arpitsojitra7179@gmail.com>
2026-06-08 11:39:41 +0530

    update index and show api

--- app/Http/Controllers/User/EventController.php
+++ app/Http/Controllers/User/EventController.php
@@ -20,7 +20,21 @@ class EventController extends Controller
     public function index(Request $request)
     {
         try {
-            return $this->eventRepository->getEvents($request);   
+            return $this->eventRepository->events($request);   
+        } catch (\Exception $e) {
+            report($e);
+
+            return response()->json([
+                'message' => 'Something went wrong.',
+            ], 500);
+        }
+    }
+
+    public function show(Event $event) {
+        try {
+            return response()->json([
+                'event' => $event,
+            ], 200);
         } catch (\Exception $e) {
             report($e);
 
