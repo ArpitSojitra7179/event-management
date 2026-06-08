a9a87a017da1bbd88eb838a11e8e0180add74bc7
ArpitSojitra7179 <arpitsojitra7179@gmail.com>
2026-06-08 17:14:36 +0530

    create admin side apis

--- routes/Admin/admin.php
+++ routes/Admin/admin.php
@@ -7,18 +7,25 @@ use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\EventController;
 
 Route::controller(AdminController::class)->middleware(['auth:api', 'IsAdmin'])->group(function () {
-    Route::get('/request-list', 'requestList');
-    Route::patch('/approve-request/{user}', 'approveRequest');
+    Route::get('/organizers/request-list', 'organizerRequests');
+    Route::get('/events/request-list', 'eventRequests');
+    Route::patch('/user-requests/{user}/{status}', 'toggle');
 });
 
 Route::controller(UserController::class)->middleware(['auth:api', 'IsAdmin'])->group( function () {
-    Route::get('/get-all-users', 'index');
-    Route::patch('/edit-user-data/{user}', 'update');
-    Route::patch('/ban-user/{user}', 'banUser');
+    Route::get('/users', 'index');
+    Route::patch('/users/{user}', 'update');
+    Route::patch('/users/{user}/{status}', 'toggle');
+    Route::get('/users/{user}', 'show');
+    Route::delete('/users/{user}', 'destroy');
 });
 
 Route::controller(EventController::class)->middleware(['auth:api', 'IsAdmin'])->group(function () {
-    Route::post('/set-event-category', 'setEventCategories');
-    Route::get('/get-event-category-list', 'eventCategoryIndex');
-    Route::get('/get-all-events', 'index');
+    Route::post('/categories', 'storeCategories');
+    Route::get('/categories', 'categoryIndex');
+    Route::get('/events', 'index');
+    Route::patch('/event-requests/{event}/{status}', 'toggle');
+    Route::get('/events/{event}', 'show');
+    Route::patch('/events/{event}', 'update');
+    Route::delete('/events/{event}', 'destroy');
 });
