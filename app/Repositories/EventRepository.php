7a934d02e164bc21673baf6e580200007d216d39
ArpitSojitra7179 <arpitsojitra7179@gmail.com>
2026-06-08 10:14:41 +0530

    add event filters for search, category, and location

--- app/Repositories/EventRepository.php
+++ app/Repositories/EventRepository.php
@@ -9,17 +9,27 @@ use App\Models\EventCategory;
 
 class EventRepository implements EventRepositoryInterface
 {
-	public function getCategories() {
-		$category = EventCategory::all();
+	public function categories() {
+		$category = EventCategory::orderByDesc('id')->cursorPaginate(10);
 
 		return $category;
 	}
 
-	public function getEvents(Request $request) {
+	public function events(Request $request) {
 
-		$events = Event::when($request['category_id'] ?? null, function ($query, $categoryId) {
-			return $query->where('category_id', $categoryId);
-		})->where('status', 'publish')->get();
+		$search = $request->query('search');
+		$category_id = $request->query('category_id');
+		$location = $request->query('location');
+
+		$events = Event::when($search, function($query) use ($search) { 
+				$query->whereAny(['title', 'description'], 'like', "%$search%");
+        })
+		->when($category_id, function($query) use ($category_id) {
+				$query->where('category_id', $category_id);
+		})
+		->when($location, function($query) use ($location) {
+				$query->where('location', 'like', "%$location%");
+		})->where('event_date', '>', now())->orderByDesc('id')->paginate();
 		
 		return $events;
 	}
