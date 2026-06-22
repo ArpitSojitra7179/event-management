2e783f43772899df71e91ac8744050bee2df3ab9
ArpitSojitra7179 <arpitsojitra7179@gmail.com>
2026-06-22 13:09:25 +0530

    add success and cancel page

---
+++ resources/views/payment/cancel.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Payment Cancelled</title>
+</head>
+<body>
+
+<div style="text-align:center;margin-top:100px;">
+
+    <h1>Payment Cancelled ❌</h1>
+
+    <p>
+        Your payment was cancelled.
+    </p>
+
+    <a href="{{ url('/') }}">
+        Try Again
+    </a>
+
+</div>
+
+</body>
+</html>
---
+++ resources/views/payment/success.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Payment Success</title>
+</head>
+<body>
+
+<div style="text-align:center;margin-top:100px;">
+
+    <h1>Payment Successful 🎉</h1>
+
+    <p>
+        Thank you for your payment.
+    </p>
+    
+    <a href="{{ url('/') }}">
+        Go Home
+    </a>
+
+</div>
+
+</body>
+</html>
