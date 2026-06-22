1072055c05f0f346478b0c6f2f13c65fc7c6e6f9
ArpitSojitra7179 <arpitsojitra7179@gmail.com>
2026-06-22 12:43:02 +0530

    add transaction and ticket booking routes

--- routes/User/user.php
+++ routes/User/user.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\User\UserController;
 use App\Http\Controllers\User\OrganizerRequestController;
 use App\Http\Controllers\User\EventController;
+use App\Http\Controllers\User\PaymentController;
+use App\Http\Controllers\User\TicketController;
 
 
 Route::controller(OrganizerRequestController::class)->group(function () {
@@ -21,4 +23,18 @@ Route::middleware('auth:api')->controller(UserController::class)->group(function
 Route::controller(EventController::class)->middleware('auth:api')->group(function () {
 	Route::get('/events', 'index');
 	Route::get('/event/{event}', 'show');
+});
+
+Route::controller(PaymentController::class)->middleware('auth:api')->group(function () {
+	Route::post('/execute/{key}', 'execute');
+	Route::post('/verify/{key}', 'verify');
+});
+
+Route::controller(PaymentController::class)->group(function () {
+	Route::get('/payment/success', 'success')->name('payment.success');
+	Route::get('/payment/cancel', 'cancel')->name('payment.cancel');
+});
+
+Route::controller(TicketController::class)->middleware('auth:api')->group(function () {
+	Route::post('/tickets/book/{event}', 'book');
 });
