c8ed25b1351699e6063372adb4168b17ab45391c
ArpitSojitra7179 <arpitsojitra7179@gmail.com>
2026-07-28 13:28:25 +0530

    Define TransactionSuccessOrFail Event in EventServiceProvider

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,6 +6,8 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use App\Events\TransactionSuccessOrFail;
+use App\Listeners\SendTransactionSuccessOrFailNotification;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -17,6 +19,10 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+        ],
+
+        TransactionSuccessOrFail::class => [
+            SendTransactionSuccessOrFailNotification::class,
         ],
     ];
 
